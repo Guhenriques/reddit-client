6542a17e9127cbae1278f82ca56f255adbf2acf9
Gustavo <guhenriques1398@gmail.com>
2023-03-17 11:50:35 -0300

    Subreddit API fetch

--- src/components/Aside/Subreddit.jsx
+++ src/components/Aside/Subreddit.jsx
@@ -1,41 +1,34 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import './Subreddit.css';
 import '../Main/Articles/MainArticle.css'
 
 const Aside = () => {
-  const subreddits = [
-    "askreddit",
-    "worldnew",
-    "videos",
-    "funny",
-    "todayilearned",
-    "pics",
-    "gaming",
-    "movies",
-    "news",
-    "gifs",
-    "aww",
-    "mildlyinteresting",
-    "showerthoughts",
-    "television",
-    "jokes",
-    "science",
-    "soccer",
-    "internetisbeautiful",
-    "dataisbeautiful",
-  ]
+  const [subReddits, setSubReddits] = useState([]); 
+
+  const fetchApi = () => {
+    fetch('https://www.reddit.com/r/popular.json')
+    .then((response) => response.json())
+    .then((data) => setSubReddits(data));
+  } 
+
+  useEffect(() => {
+    fetchApi()
+  }, [])
+
+  console.log('subReddit:', subReddits?.data?.children)
 
   return (
     <div className='subreddit-container'>
       <h2>Subreddits</h2>
       <ul className='subreddit-list'>
-        {subreddits.map(subreddit => (
-          <li><a href={`/r/${subreddit}`}>{subreddit}</a></li>
+        {subReddits?.data?.children.map(subReddit => ( 
+          <li><a href={`${subReddit.data.permalink}`}>{subReddit.data.subreddit}</a></li>
         ))}
       </ul>
-
     </div>
   )
 }
 
 export default Aside;
+
